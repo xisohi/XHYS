0acc3185c2cd599318b8dadd6600661cd89b62f4
xihi <xuhui0607@gmail.com>
2024-01-15 21:52:30 +0800

    多

--- app/src/main/java/com/github/tvbox/osc/ui/dialog/AboutDialog.java
+++ app/src/main/java/com/github/tvbox/osc/ui/dialog/AboutDialog.java
@@ -1,8 +1,11 @@
 package com.github.tvbox.osc.ui.dialog;
 
 import android.content.Context;
+
 import androidx.annotation.NonNull;
+
 import com.github.tvbox.osc.R;
+
 import org.jetbrains.annotations.NotNull;
 import android.widget.TextView;
 import android.os.Bundle;
